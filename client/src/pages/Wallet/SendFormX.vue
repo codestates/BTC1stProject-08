8e005a8e963ca426051df29ab117f6bbe6163bed
jiyoung-99 <1skylar1111@gmail.com>
2022-02-25 21:35:34 +0900

    feat: X chain 송금 기능 구현
    스토어에 있는 계정으로 송금 기능 구현

--- client/src/pages/Wallet/SendFormX.vue
+++ client/src/pages/Wallet/SendFormX.vue
@@ -1,13 +1,6 @@
 <template>
   <card>
     <div class="row">
-<!--      <div class="col-md-10 pr-md-1">-->
-<!--        <base-input label="Amount"-->
-<!--                  placeholder="Amount"-->
-<!--                  v-model="model.amount"-->
-<!--                  disabled>-->
-<!--        </base-input>-->
-<!--      </div>-->
       <div class="col-md-4 px-md-1">
             <label>Amount</label>
             <input class="form-control"
@@ -19,7 +12,6 @@
       <div class="col-md-1 pl-md-1">
         <base-input label="Token"
                     placeholder="AVAX"
-                    v-model="token"
                     disabled>
         </base-input>
       </div>
@@ -46,18 +38,13 @@
       </div>
     </div>
     <div class="row">
-      <label>Transaction Fee&nbsp;&nbsp;&nbsp;</label>
-      <p>10</p>
-    </div>
-    <div class="row">
-      <label>Total AVAX&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-      <p>10</p>
+      <label>Transaction Fee: &nbsp;&nbsp;0.001 AVAX</label>
     </div>
     <base-button :loading = "isLoading" slot="footer" type="primary" @click="deposit">Send</base-button>
   </card>
 </template>
 <script>
-import {setNetwork, MnemonicWallet, BN, TestnetConfig} from "@avalabs/avalanche-wallet-sdk";
+import {setNetwork, numberToBNAvaxX } from "@avalabs/avalanche-wallet-sdk";
 
 export default {
     name: 'SendFormX',
@@ -65,25 +52,22 @@ export default {
       return {
         isLoading: false,
         amount: 0.01,
-        token: 'AVAX',
         to_address: '',
         memo: '',
-        transactionFee: 0,
-        totalAvax: 0
       };
     },
     methods: {
       async deposit() {
         try {
           this.isLoading = true;
-          const newMnemonic = 'fantasy trim fun junk wisdom cement rally infant blush twist mom dilemma museum flat dad bulk hunt lawn unable huge sunny almost equip song';
-          const myWallet = MnemonicWallet.fromMnemonic(newMnemonic);
-          setNetwork(TestnetConfig);
+          const myWallet = this.$store.state.wallet;
+          setNetwork(this.$store.state.currentNetwork);
           await myWallet.resetHdIndices();
           await myWallet.updateUtxosX();
-          const amount = new BN(this.amount);
-          // this.transactionFee = transa
+          const amount = numberToBNAvaxX(this.amount);
           await myWallet.sendAvaxX(this.to_address, amount, this.memo);
+          this.isLoading = false;
+          alert('성공적으로 전송이 완료되었습니다.');
         } catch (error) {
           console.info(error);
           this.isLoading = false;
