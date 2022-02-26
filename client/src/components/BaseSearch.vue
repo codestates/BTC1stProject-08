51cfac01dc8c390e16f09358029d0206a317cb50
jiyoung-99 <1skylar1111@gmail.com>
2022-02-26 21:41:22 +0900

    feat: explorer 페이지 추가

---
+++ client/src/components/BaseSearch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="search-bar input-group">
+    <input type="text" class="form-control" :value="keyword" @keyup.enter="$emit('click')" placeholder="Search by Address / Txn Hash / Block / Token">
+    <div class="input-group-addon" @click="$emit('click')"><i class="tim-icons icon-zoom-split"></i></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "base-search",
+  props: {
+    keyword: {
+      type: [String, Number],
+    },
+  },
+};
+</script>
+<style lang="scss">
+.search-bar.input-group {
+  border: 1px solid;
+  height: 40px;
+  margin-left: 0;
+  margin-bottom: 1rem;
+  padding-left: .5rem;
+
+  .input-group-addon {
+    padding: 8px 15px 10px 10px;
+    cursor: pointer;
+  }
+
+  input::-webkit-input-placeholder {
+    opacity: 0.5; /*Change the opacity between 0 and 1*/
+  }
+}
+</style>
